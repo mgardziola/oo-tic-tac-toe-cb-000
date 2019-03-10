5bd7a3fb58ce22ba89c6b34d1c8434201c54e03e
mariah <mariah@gardziola.com>
2019-03-10 23:46:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ class TicTacToe
   
   def turn
     
-    puts "Please enter 1-9:"
+
     user_input = gets.strip
     
     index = input_to_index(user_input)
@@ -66,6 +66,7 @@ class TicTacToe
       move(index, token)
       display_board
     else 
+          puts "Please enter 1-9:"
       turn 
     end 
   end
